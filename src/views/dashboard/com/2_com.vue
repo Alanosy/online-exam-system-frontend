f697780f68c1262bcedc4498011c8434b84e6c51
st <2946594574@qq.com>
2024-04-11 13:53:16 +0800

    第一次合

--- src/views/dashboard/com/2_com.vue
+++ src/views/dashboard/com/2_com.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: st 2946594574@qq.com
+ * @Date: 2024-03-04 15:23:49
+ * @LastEditors: st 2946594574@qq.com
+ * @LastEditTime: 2024-04-08 15:59:06
+ * @FilePath: \com-project\src\views\dashboard\com\2_com.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div>我是学生</div>
 </template>
