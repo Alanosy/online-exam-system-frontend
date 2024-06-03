4ca74b54531a66f91561872d0c01c345a3feccda
st <2946594574@qq.com>
2024-06-03 15:51:09 +0800

    603

--- src/views/repo/index.vue
+++ src/views/repo/index.vue
@@ -2,7 +2,7 @@
  * @Author: yangiiiiii 14122140+yangiiiiiii@user.noreply.gitee.com
  * @Date: 2024-04-01 11:00:21
  * @LastEditors: st 2946594574@qq.com
- * @LastEditTime: 2024-05-11 16:05:47
+ * @LastEditTime: 2024-06-03 15:49:27
  * @FilePath: \com-project\src\views\notice\notice.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -94,7 +94,7 @@
     >
       <el-row :gutter="20">
         <el-col>
-          <el-form :model="addTitle">
+          <el-form>
             <el-form-item label="题库名称:" :label-width="formLabelWidth">
               <el-input
                 v-model="addTitle"
@@ -253,6 +253,7 @@ export default {
         })
         .catch((_) => {})
     },
+
     searchRepo() {
       this.getRepoPage(this.pageNum, this.pageSize, this.searchTitle)
     },
