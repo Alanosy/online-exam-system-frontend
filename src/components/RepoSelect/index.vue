d3c434c06df21b1d62b63bc1eae086a65e2ecf98
魏进 <3413105907@qq.com>
2024-06-03 22:09:56 +0800

    6/3

--- src/components/RepoSelect/index.vue
+++ src/components/RepoSelect/index.vue
@@ -2,7 +2,7 @@
  * @Author: 魏进 3413105907@qq.com
  * @Date: 2024-05-28 12:26:25
  * @LastEditors: 魏进 3413105907@qq.com
- * @LastEditTime: 2024-05-28 20:15:34
+ * @LastEditTime: 2024-06-03 21:51:15
  * @FilePath: \online-exam-system-frontend\src\components\RepoSelect\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
--- src/views/score/index.vue
+++ src/views/score/index.vue
@@ -2,7 +2,7 @@
  * @Author: yangiiiiii 14122140+yangiiiiiii@user.noreply.gitee.com
  * @Date: 2024-04-01 11:00:21
  * @LastEditors: 魏进 3413105907@qq.com
- * @LastEditTime: 2024-05-31 02:05:11
+ * @LastEditTime: 2024-06-03 21:05:39
  * @FilePath: \com-project\src\views\notice\notice.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -98,6 +98,8 @@ export default {
       updateRow(row) {
         localStorage.setItem('examId', row.examId)
         localStorage.setItem('gradeId', row.gradeId)
+        localStorage.setItem('examTitle', row.examTitle)
+        localStorage.setItem('gradeName', row.gradeName)
         this.$router.push({ name: 'UserScore' })
       },
       diaTitle: '',
