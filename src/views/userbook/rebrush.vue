e632c29e8ba6c558553409010cb2081e99f3d1df
Alan <alanos@aliyun.com>
2025-04-02 23:28:09 +0800

    修复错题本
    修复错题本错题本的重刷,当第一道题的回答是错的，后续所有作答就会一直显示是错的，当第一道题的回答是对的，后续所有的作答就会是对的，并且移除题目

--- src/views/userbook/rebrush.vue
+++ src/views/userbook/rebrush.vue
@@ -1,5 +1,12 @@
 <template>
-  <div style="width: 100%; height: 100%; background-color: #f0f2f5; padding: 20px 0 0">
+  <div
+    style="
+      width: 100%;
+      height: 100%;
+      background-color: #f0f2f5;
+      padding: 20px 0 0;
+    "
+  >
     <el-row :gutter="24">
       <el-col :span="24">
         <el-card style="margin-bottom: 10px">
@@ -31,22 +38,30 @@
                 v-for="item in quData.answerList"
                 :key="item.id"
                 :label="item.id"
-              >{{ numberToLetter(item.sort) }}.{{ item.content }}
-                <div v-if="item.image != null && item.image != ''" style="clear: both">
+                >{{ numberToLetter(item.sort) }}.{{ item.content }}
+                <div
+                  v-if="item.image != null && item.image != ''"
+                  style="clear: both"
+                >
                   <el-image :src="item.image" style="max-width: 200px" />
                 </div>
               </el-radio>
             </el-radio-group>
           </div>
-          <div v-if="flag == true && (quData.quType === 1 || quData.quType === 3)">
+          <div
+            v-if="flag == true && (quData.quType === 1 || quData.quType === 3)"
+          >
             <!-- <div>
               <span>我的答案:{{ myAnswers }}</span>
             </div> -->
             <div>
-              <span>正确答案:  {{ numberToLetter(parseInt(failQuData.rightAnswers)) }}</span>
+              <span
+                >正确答案:
+                {{ numberToLetter(parseInt(failQuData.rightAnswers)) }}</span
+              >
             </div>
             <div>
-              <span>试题分析:  {{ failQuData.analysis }}</span>
+              <span>试题分析: {{ failQuData.analysis }}</span>
             </div>
           </div>
           <div v-if="quData.quType === 2">
@@ -55,8 +70,11 @@
                 v-for="item in quData.answerList"
                 :key="item.id"
                 :label="item.id"
-              >{{ numberToLetter(item.sort) }}.{{ item.content }}
-                <div v-if="item.image != null && item.image != ''" style="clear: both">
+                >{{ numberToLetter(item.sort) }}.{{ item.content }}
+                <div
+                  v-if="item.image != null && item.image != ''"
+                  style="clear: both"
+                >
                   <el-image :src="item.image" style="max-width: 200px" />
                 </div>
               </el-checkbox>
@@ -68,10 +86,12 @@
               <span>我的答案:{{ myAnswers }}</span>
             </div> -->
             <div>
-              <span>正确答案:  {{ numberToLetter(failQuData.rightAnswers) }}</span>
+              <span
+                >正确答案: {{ numberToLetter(failQuData.rightAnswers) }}</span
+              >
             </div>
             <div>
-              <span>试题分析:  {{ failQuData.analysis }}</span>
+              <span>试题分析: {{ failQuData.analysis }}</span>
             </div>
           </div>
 
@@ -94,15 +114,18 @@
               </el-checkbox> -->
             <!-- </el-checkbox-group> -->
           </div>
-          <div v-if="flag == true && quData.quType === 4" style="margin-top: 10px;">
+          <div
+            v-if="flag == true && quData.quType === 4"
+            style="margin-top: 10px"
+          >
             <!-- <div>
               <span>我的答案:{{ myAnswers }}</span>
             </div> -->
             <div>
-              <span>正确答案:  {{ failQuData.rightAnswers }}</span>
+              <span>正确答案: {{ failQuData.rightAnswers }}</span>
             </div>
             <div>
-              <span>试题分析:  {{ failQuData.analysis }}</span>
+              <span>试题分析: {{ failQuData.analysis }}</span>
             </div>
           </div>
           <div style="margin-top: 20px">
@@ -119,119 +142,120 @@
   </div>
 </template>
 <script>
-import { fullBook, getSingleQu, getUserBookList } from '@/api/userbook'
+import { fullBook, getSingleQu, getUserBookList } from "@/api/userbook";
 export default {
   data() {
     return {
-      repoId: '',
-      repoTitle: '',
+      repoId: "",
+      repoTitle: "",
       routeData: {},
       // 全屏/不全屏
       isFullscreen: false,
       showPrevious: false,
       showNext: true,
       loading: false,
-      handleText: '提交',
+      handleText: "提交",
       pageLoading: false,
       nextText: false,
       userBookList: [],
       index: 0,
       quDataLen: 0,
-      examId: '',
+      examId: "",
       failQuData: {},
       flag: false,
       // 当前题目内容
       quData: {
-        answerList: []
+        answerList: [],
       },
       // 试卷信息
-      radioValue: '',
+      radioValue: "",
       showAnswer: false,
       // 多选选定值
       multiValue: [],
       // 已答ID
       answeredIds: [],
-      saqTextarea: '',
-      myAnswers: '',
+      saqTextarea: "",
+      myAnswers: "",
       lastIndex: 0,
       paperData: {
         leftSeconds: 99999,
         radioList: [],
         multiList: [],
-        judgeList: []
-      }
-    }
+        judgeList: [],
+      },
+    };
   },
   created() {
-    this.routeData = this.$route.query.zhi
-    this.examId = localStorage.getItem('userbook_examId')
-    this.getUserBookListFun()
+    this.routeData = this.$route.query.zhi;
+    this.examId = localStorage.getItem("userbook_examId");
+    this.getUserBookListFun();
     // this.getSingleQuFun()
   },
 
   methods: {
     handHandExam() {
-      const that = this
+      const that = this;
       // 交卷保存答案
-      const msg = '确认要提交吗？'
+      const msg = "确认要提交吗？";
       that
-        .$confirm(msg, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
+        .$confirm(msg, "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
         })
         .then(() => {
-          this.$router.push({ name: 'wrong-book' })
+          this.$router.push({ name: "wrong-book" });
         })
         .catch(() => {
           that.$message({
-            type: 'info',
-            message: '交卷已取消，您可以继续作答！'
-          })
-        })
+            type: "info",
+            message: "交卷已取消，您可以继续作答！",
+          });
+        });
     },
 
     getSingleQuFun(quId) {
       getSingleQu(quId).then((res) => {
-        this.quData = res.data
-      })
+        this.quData = res.data;
+      });
     },
     getUserBookListFun() {
       getUserBookList(this.examId).then((res) => {
-        this.userBookList = res.data
-        this.quDataLen = res.data.length
-        this.getSingleQuFun(res.data[this.index]['quId'])
-        this.lastIndex = this.userBookList.length
-      })
+        this.userBookList = res.data;
+        this.quDataLen = res.data.length;
+        this.getSingleQuFun(res.data[this.index]["quId"]);
+        this.lastIndex = this.userBookList.length;
+      });
     },
     numberToLetter(input) {
       const numberToCharMap = {
-        0: 'A',
-        1: 'B',
-        2: 'C',
-        3: 'D',
-        4: 'E',
-        5: 'F'
-      }
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D",
+        4: "E",
+        5: "F",
+      };
 
       // 辅助函数：将单个数字（字符串或数字类型）转换为字母
-      const singleNumberToLetter = (num) => numberToCharMap[parseInt(num, 10)] || ''
+      const singleNumberToLetter = (num) =>
+        numberToCharMap[parseInt(num, 10)] || "";
 
       // 辅助函数：处理逗号分隔的数字字符串
       const commaSeparatedNumbersToLetters = (str) => {
-        const numbers = str.split(',').map((item) => parseInt(item.trim(), 10))
-        return numbers.map((number) => numberToCharMap[number] || '').join(',')
-      }
+        const numbers = str.split(",").map((item) => parseInt(item.trim(), 10));
+        return numbers.map((number) => numberToCharMap[number] || "").join(",");
+      };
 
       // 判断输入类型并调用相应函数
       if (/^\d+$/.test(input)) {
         // 单个数字（字符串形式也可以匹配）
-        return singleNumberToLetter(input)
+        return singleNumberToLetter(input);
       } else if (/^\d+(,\d+)*$/.test(input)) {
         // 包含逗号分隔的数字字符串
-        return commaSeparatedNumbersToLetters(input)
+        return commaSeparatedNumbersToLetters(input);
       } else {
-        return '' // 输入不符合预期，返回空字符串或根据需要处理
+        return ""; // 输入不符合预期，返回空字符串或根据需要处理
       }
     },
     /**
@@ -239,13 +263,13 @@ export default {
      */
     handNext() {
       if (!this.flag) {
-        this.index = this.index + 1
-        this.handSave(this.index)
+        this.index = this.index + 1;
+        this.handSave(this.index);
       } else {
         if (this.index >= this.lastIndex) {
-          this.handHandExam()
+          this.handHandExam();
         }
-        this.handSave(this.index)
+        this.handSave(this.index);
       }
     },
 
@@ -253,67 +277,73 @@ export default {
      * 上一题
      */
     handPrevious() {
-      this.index = this.index - 1
-      this.handSave(this.index)
+      this.index = this.index - 1;
+      this.handSave(this.index);
     },
     // 保存答案
     handSave(index) {
       if (index - 1 >= this.lastIndex) {
-        this.handHandExam()
+        this.handHandExam();
       } else {
-        const answers = this.multiValue
-        if (this.radioValue  != '') {
-          answers.push(this.radioValue)
+        let answer;
+        if (this.quData.quType === 2) {
+          // 多选题
+          answer = this.multiValue.join(",");
+        } else if (this.quData.quType === 1 || this.quData.quType === 3) {
+          // 单选题或判断题
+          answer = this.radioValue;
+        } else if (this.quData.quType === 4) {
+          // 简答题
+          answer = this.saqTextarea;
         }
 
         const params = {
           examId: this.examId,
-          quId: this.userBookList[index - 1]['quId'],
-          answer: this.quData.quType === 4 ? this.saqTextarea : answers.join(',')
-        }
+          quId: this.userBookList[index - 1]["quId"],
+          answer: answer,
+        };
 
         // this.myAnswers = params.answer;
         if (!this.flag) {
           fullBook(params).then((res) => {
             if (res.code) {
-              this.failQuData = res.data
-
+              this.failQuData = res.data;
               if (res.data.correct) {
                 this.$message({
-                  type: 'success',
-                  message: res.msg
-                })
+                  type: "success",
+                  message: res.msg,
+                });
               } else {
                 this.$message({
-                  type: 'error',
-                  message: res.msg
-                })
+                  type: "error",
+                  message: res.msg,
+                });
               }
             } else {
               this.$message({
-                type: 'error',
-                message: res.msg
-              })
+                type: "error",
+                message: res.msg,
+              });
             }
-          })
+          });
         }
 
         if (this.flag === true) {
           // 查找详情
-          this.fetchQuData(index)
-          this.flag = false
+          this.fetchQuData(index);
+          this.flag = false;
         } else {
-          this.flag = true
+          this.flag = true;
         }
       }
       // });
     },
     // 试卷详情
     fetchQuData(index) {
-      this.getSingleQuFun(this.userBookList[index]['quId'])
-    }
-  }
-}
+      this.getSingleQuFun(this.userBookList[index]["quId"]);
+    },
+  },
+};
 </script>
 <style scoped>
 page {
