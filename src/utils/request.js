be54681794c832fb7ed7055e931eb8b16cc9d3af
狗蛋 <moxuec@gmail.com>
2025-05-30 10:14:52 +0800

    增加请求超时时间，避免题库较大时出现超时

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ import router from '@/router'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000, // request timeout
+  timeout: 30000, // request timeout
   crossDomain: true
 })
 
